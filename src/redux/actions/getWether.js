bb98c3d84820ca0739779b8713304d853755e96f
Alka-Rautela-au8 <alka.rautela@icloud.com>
2020-11-28 17:26:32 +0530

    note app

--- src/redux/actions/getWether.js
+++ src/redux/actions/getWether.js
@@ -6,7 +6,7 @@ const key = 'b1753b8e188a5600bcad02a8227612ee'
 export const fetchWeather = (city) => async(dispatch) => {
     try{
         dispatch({type: NOT_FOUND,  payload: false})
-        const {data} = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`) 
+        const {data} = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`) 
         console.log(data)
         dispatch({type: FETCH_SEARCHED_WEATHER, payload: data})
     }catch(err){
